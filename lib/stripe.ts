9eb912761b27e8f2db4cca7c81beaccd80b85233
Alexandre Macedo Costa <alemacedo@gmail.com>
2026-03-25 17:10:00 -0300

    fix: remove invalid Stripe apiVersion to fix TypeScript build error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -8,9 +8,7 @@ const stripeProxy = new Proxy({} as Stripe, {
       if (!process.env.STRIPE_SECRET_KEY) {
         throw new Error("STRIPE_SECRET_KEY is not set");
       }
-      _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: "2025-03-31.basil",
-      });
+      _stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
     }
     return (_stripe as any)[prop];
   },
